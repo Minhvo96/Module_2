640d8c3ea0f0a934223027953d2b730feb745500
Minhvo96 <voquangminhbck38@gmail.com>
2023-06-07 10:47:20 +0700

    update

--- Exam_1/src/main/java/Exam/Bt11.java
+++ Exam_1/src/main/java/Exam/Bt11.java
@@ -9,20 +9,23 @@ public class Bt11 {
         int[] numbers = {1, 2, 3, 4, 5, 4, 3, 2, 1};
         System.out.println("Các phần tử có trong mảng là:");
         System.out.println(Arrays.toString(numbers));
-        if (symmetricArray(numbers) == 1) {
-            System.out.println("Mảng này đối xứng");
+        if (symmetricArray(numbers) == true) {
+            System.out.println("1. Mảng này đối xứng");
         } else {
-            System.out.println("Mảng này không đối xứng");
+            System.out.println("0. Mảng này không đối xứng");
         }
     }
 
-    public static int symmetricArray(int[] array) {
+    public static boolean symmetricArray(int[] array) {
+        boolean flag = true;
+
         for (int i = 0; i < array.length / 2; i++) {
              int j = array.length - 1 - i;
-                if (array[i] == array[j]) {
-                    return 1;
+                if (array[i] != array[j]) {
+                    flag = false;
+                    return flag;
                 }
             }
-        return 0;
+        return flag;
     }
 }
